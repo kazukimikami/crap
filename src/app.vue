3cfd16dc69402e5407310df299cfc511fbd97eb1
Kazuki Mikami <kazuki.mikami@devenues.com>
2023-06-02 16:14:38 +0900

    ヘッダー、フッターをNuxtLayoutsから呼び出し

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,20 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+
+    <div class="form">
+        <input v-model="taskName">
+        <p>{{ taskName }}</p>
+    </div>
+
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const taskName = ref<string>('')
+</script>
+
+<style scoped>
+.form {
+  text-align: center;
+}
+</style>
