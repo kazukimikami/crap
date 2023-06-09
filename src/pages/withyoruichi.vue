2c8c8091e3116ea9e482674c2be8ce50c1293048
Kazuki Mikami <kazuki.mikami@devenues.com>
2023-06-09 15:55:12 +0900

    ui変更

--- src/pages/withyoruichi.vue
+++ src/pages/withyoruichi.vue
@@ -1,12 +1,14 @@
 <template>
+    <div class="yoruichi">
+        <img src="@/assets/img/yoruichi.png">
+        <div class="box">
+            {{ generateText }}
+        </div>
+    </div>
     <form class="form" @submit.prevent="handleClick">
         <input class="input" type="text" placeholder="Let's talk!" v-model="keyword" required>
         <button class="button" type="submit">Talk</button>
     </form>
-    <div class="yoruichi">
-        <p><img src="https://pbs.twimg.com/media/E854tX9VoAIi1px.png"></p>
-        <p>{{ generateText }}</p>
-    </div>
 </template>
 
 <script setup lang="ts">
@@ -49,21 +51,22 @@ const handleClick = async () => {
 </script>
 
 <style scoped>
+.yoruichi {
+    margin: 0 auto;
+    text-align: center;
+    max-width: 60%;
+}
+.yoruichi > img {
+    width: 45%;
+    margin: 2%;
+}
 .form {
     text-align: center;
     padding: 20px;
 }
 input {
-    width: 40%;
+    width: 45%;
     margin-bottom: 20px;
-}
-.yoruichi {
-    display: flex;
-    justify-content: center;
-}
-.yoruichi>p {
-    width: 20%;
-    margin: 2%;
 }
 </style>
 
